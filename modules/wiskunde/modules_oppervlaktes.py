4fe0eedb9c60f76ed2fd9a6edb9f88125a205958
bjornlecis <bjorn.lecis@gmail.com>
2023-02-01 11:58:58 +0100

    einde les

---
+++ modules/wiskunde/modules_oppervlaktes.py
@@ -0,0 +1,4 @@
+def oppv_driehoek(b,h):
+    return b*h/2
+def oppv_vierkant(z):
+    return z*z
---
+++ test.py
@@ -0,0 +1,16 @@
+"""from modules.wiskunde import modules_oppervlaktes as mo
+
+driehoek = mo.oppv_driehoek(4,3)
+print(driehoek)
+
+import platform
+
+x = platform.system()
+print(x)
+"""
+import math
+"""
+for functie in dir(math):
+    print(functie)
+"""
+help(print)
